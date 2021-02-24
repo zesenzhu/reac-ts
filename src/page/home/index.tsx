878066734a0de99b35c7cbf3d31c5b993a4ab5cc
zhuzesen <2362039373@qq.com>
2021-02-25 00:47:43 +0800

    ''

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -6,17 +6,38 @@
  * @Description: 祝泽森
  * @FilePath: \TypeScript\react-ts\src\page\home\index.tsx
  */
-import React, { memo, forwardRef, } from "react";
+import React, {  forwardRef, useState, useCallback } from "react";
+import {  Drawer,  NavBar, Icon } from 'antd-mobile';
 interface Props {
   children?: React.ReactNode
 }
 
 const Home: React.ForwardRefRenderFunction<HTMLDivElement, Props> = (props: Props, ref) => {
-  enum testEnum {
-    a=10, b, c, d
-  }
-console.log(testEnum)
-  return <p>这是ts</p>;
+  // enum testEnum {
+  //   a = 10, b, c, d
+  // }
+  const [Open, setOpen] = useState(false);
+  const onOpenChange = useCallback(
+    () => {
+
+      setOpen(pre => {
+        console.log(pre)
+        return !pre})
+    },
+    [Open],
+  )
+  return <div ><NavBar icon={<Icon onClick={onOpenChange} type="ellipsis" />} onLeftClick={onOpenChange}>Basic</NavBar>
+    <Drawer
+      className="my-drawer"
+      style={{ minHeight: document.documentElement.clientHeight }}
+      enableDragHandle
+      contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+      sidebar={<div style={{height:'100%',width:'100%'}}></div>}
+      open={Open}
+      onOpenChange={onOpenChange}
+    >
+      Click upper-left corner
+  </Drawer></div>
 };
 
 export default forwardRef(Home)
